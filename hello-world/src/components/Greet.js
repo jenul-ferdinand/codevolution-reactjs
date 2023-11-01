298332cebea69b9ce7c72cec93b24911bbd1864a
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2023-11-01 19:03:22 +1100

    6 Class Components

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
-// Functional component concise arrow function syntax with a named export
-export const Greet = () => <h1>Hello Jenul</h1>
+// Functional Component
+const Greet = () => <h1>Hello Jenul</h1>
 
-// Functional component with a default export
-function Greet2() {
-    return <h1>Hello Jenul</h1>
-}
+// Export the component
+export default Greet;
 
-export default Greet2;
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// Class component Welcome
+class Welcome extends Component {
+    render() {
+        return (
+            <h1>Class Component</h1>
+        );
+    }
+}
+
+// Export the Welcome class component
+export default Welcome;
