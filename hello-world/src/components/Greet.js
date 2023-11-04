d28eb022f265d67a4aad5aef975a7f9c58636076
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2023-11-04 23:19:47 +1100

    9 Props

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 // Functional Component
-const Greet = () => <h1>Hello Jenul</h1>
+const Greet = props => {
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+            {props.children}
+        </div>
+    );
+}
 
 // Export the component
 export default Greet;
