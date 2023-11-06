1d46fa288ab108679a6b0ddd87063ce858f1cb08
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2023-11-07 01:34:48 +1100

    11 setState

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg'
 import './App.css'
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -31,6 +31,11 @@ function App() {
       <br/>
 
       <Message/>
+
+      <br/> 
+      <br/>
+
+      <Counter/>
     </div>
   )
 }
