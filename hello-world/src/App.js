98216102b2d044c5fac2eada501b394d0e03bc69
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2023-12-04 23:56:32 +1100

    13 Event Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,10 +4,15 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick/>
+      <ClassClick/>
+
       <Hello/>
 
       <br/>
